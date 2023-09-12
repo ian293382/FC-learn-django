7186decc8f4a99689106cf54ebb301e7b1ee79be
ian293382 <moshou5571228@gmail.com>
2023-09-13 03:44:05 +0800

    show item_name

--- item/models.py
+++ item/models.py
@@ -16,8 +16,11 @@ class Item(models.Model):
     category = models.ForeignKey(Category, related_name='items', on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    price =models.FloatField()
+    price = models.FloatField()
     image = models.ImageField(upload_to='item_images', blank=True, null=True)
     is_sold = models.BooleanField(default=False)
-    created_at_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return self.name
